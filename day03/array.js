0ff93c1bb4bdf9cb3a4223700a0ce7e91805a33f
Tushar Singhal <72409849+tsinghal451@users.noreply.github.com>
2024-04-02 10:09:36 +0000

    ADD SOME NOTES

--- day03/array.js
+++ day03/array.js
@@ -8,32 +8,34 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.push(6) //0,1,2,3,4,5,6
+myArr.push(7) // 0,1,2,3,4,5,6,7
+myArr.pop()   //0,1,2,3,4,5,6
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.unshift(9) // 9,0,1,2,3,4,5,6
+myArr.shift()    // 0,1,2,3,4,5,6
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr);
 
-// const newArr = myArr.join()
+console.log(myArr.includes(9)); // FALSE
+console.log(myArr.indexOf(3));  // 3
+
+const newArr = myArr.join()
 
 // console.log(myArr);
-// console.log( newArr);
+console.log( newArr); // BINDING THE ARRAY AND CONVERT INTO STRING
 
 
 // slice, splice
 
-console.log("A ", myArr);
+//console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+//console.log(myn1);
+//console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+//console.log("C ", myArr);
+//console.log(myn2);
