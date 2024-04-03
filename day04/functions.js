3292c9cf9a5d90b9a2f9f72c4eabe1e8911da3c4
Tushar Singhal <72409849+tsinghal451@users.noreply.github.com>
2024-04-03 07:53:24 +0000

    add new files

---
+++ day04/functions.js
@@ -0,0 +1,61 @@
+// functions 
+
+function add(number1 , number2){
+    let result = number1 + number2
+    return result 
+}
+console.log(add(3,4)) // 7
+
+let ans = add(4,8)
+console.log(ans); // 12
+
+
+
+function loginuser(username = "sam"){
+    if(!username){
+        console.log("please logged in");
+        return
+    }
+    return `${username} just logged in`
+}
+
+loginuser("tushar") // nothing print but function will be call
+let log = loginuser("tushar")
+console.log(log); //tushar just logged in
+console.log(loginuser()) // please logged in undefined but if we pass default value in parameter than it will print //sam just logged in always  
+
+
+function calculateCartPrize(...num1){ // ... this is a rest operator it helps to store vlaue in a bundle as a array
+      
+    return num1
+}
+
+console.log(calculateCartPrize(200,400,3000)) // [ 200, 400, 3000 ]
+
+
+const user ={
+    username : "tushar singhal",
+    price : 80
+}
+
+function handleObjeect(anyobject){
+    console.log(`username is ${anyobject.username} and the price is ${anyobject.price}`)
+}
+
+handleObjeect(user) //username is tushar singhal and the price is 80
+
+handleObjeect({
+    username: "tanmay",
+    price: 499
+}) // username is tanmay and the price is 499
+
+const myNewArray = [200, 500, 300, 1000]
+
+function WorkOnArray(getArray){
+     return getArray[2]
+}
+
+console.log(WorkOnArray(myNewArray)); // 300
+console.log(WorkOnArray([300,400])); // undefined
+console.log(WorkOnArray([400,300,700, 20000])); //700
+
---
+++ day04/scope.js
@@ -0,0 +1,66 @@
+const a = 20
+{
+    const a = 30 // 30
+    console.log(a);
+}
+console.log(a) // 20
+
+let b = 40
+var c = 30
+const d = 70
+
+if(true){
+
+    let b = 30
+    var c = 50
+    const d = 80
+}
+
+console.log(b) // 40
+console.log(c); // 50 var take a value from block scope 
+console.log(d); // 70
+
+
+
+function one (){
+
+    let name = "tushar"
+
+    function two(){
+        let name2 = "hitesh"
+        console.log(name);
+    }
+
+    // console.log(name2);  // ReferenceError: name2 is not defined
+
+    two()
+
+}
+one()
+
+
+if(true){
+    const name = "tushar"
+
+    if(name === "tushar"){
+        const company = "youtube"
+        console.log(name + company); // tushar youtube
+    }
+   // console.log(company);
+}
+// console.log(name); //ReferenceError: name is not defined
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(4)) // 5
+function addOne(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5)); //ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(7)) // 9
