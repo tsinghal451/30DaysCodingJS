7c8ac7911faf004b2bfe3c9374f8204a1117d50e
Tushar Singhal <72409849+tsinghal451@users.noreply.github.com>
2024-04-04 06:02:33 +0000

    add files

---
+++ day05/highOrderLoops.js
@@ -0,0 +1,88 @@
+// forOf
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    console.log(num)
+}
+
+const greetings = "hello World"
+for (const greet of greetings) {
+    console.log(greet);
+}
+
+// Maps :- duplicate value nhi hoti , key pairs pe kaam krta hai
+
+const map = new Map()
+map.set("in" , "INDIA")
+map.set("usa", "UNITED STATES")
+
+console.log(map)
+
+for (const [key , value] of map) {
+    console.log(key , value);
+}
+
+const myObj = {
+    game1 : "nfs",
+    game2  : "pubg",
+}
+
+// for(const [key,value] of myObj){ // object are not iterable with the for of loop
+//     console.log(key,value)
+// }
+
+// forIn loop
+for (const key in myObj) {
+    console.log(`${key} shorcut is for ${myObj[key]}`);
+}
+
+const program = ["js" , "css", "html"]
+for (const key in program) {
+   console.log(key,program[key]);
+}
+
+// array -> for OF
+// object -> for in
+
+
+// For Each 
+const coding = ["js", "css", "html", "c++"]
+coding.forEach(function (items){ // foreach meh function lete hai jiska koi naam nhi hota
+   console.log(items);
+})
+
+coding.forEach( (val)=> { // arrow function
+    console.log(val);
+})
+
+
+function printme(items){
+    console.log(items);
+}
+coding.forEach(printme)
+
+coding.forEach((item , index , arr) => { // parameter use in forEach
+    console.log(item,index,arr) 
+})
+
+
+
+const myCoding = [
+    {
+        langaugeName : "javascript",
+        langaugeFileName : "java"
+    },
+    {
+        langaugeName : "c++",
+        langaugeFileName : "java"
+    },
+    {
+        langaugeName : "html",
+        langaugeFileName : "java"
+    },
+]
+
+// console.log(myCoding);
+
+myCoding.forEach( (items)=> {
+   console.log(items.langaugeFileName , items.langaugeName);
+})
---
+++ day05/loops.js
@@ -0,0 +1,54 @@
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(" ");
+    for (let j = 0; j <= 10; j++) {
+        console.log( i + '*' + j + '=' + i*j);
+    }
+}
+
+// break and continue 
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    if(i ==5){
+        console.log("detected"); 
+        break; // break statement break the loop uske baad value aage print nhi hoti
+    } 
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    if(i ==5){
+        console.log("detected"); 
+        continue; // ek baar ke liye skip kr dege isme loop se bhar nhi aate
+    } 
+}
+
+// while loop 
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArr = ["tushar", "singhal","hello"]
+let arr = 0
+while (arr < myArr.length) {
+    console.log(`Value is ${myArr[arr]}`);
+    // arr++
+    arr = arr +1
+}
+
+// do while loop
+
+let score = 0
+do {
+    console.log(score);
+    score++
+} while (score <= 10);
